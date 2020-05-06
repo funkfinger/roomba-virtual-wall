3a73bf525ab6224afe1bc097249a064cc11dfea8
Jay Wiggins <jay@irk.com>
2020-05-06 12:13:19 -0700

    simple code working

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// see: https://gist.github.com/SeeJayDee/caa9b5cc29246df44e45b8e7d1b1cdc5
+#include <tiny_IRremote.h>
+
+IRsend irsend;
+
+void setup()
+{
+  irsend.enableIROut(38);
+  pinMode(PIN1, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  irsend.mark(1000);
+  irsend.space(1000);
 }
